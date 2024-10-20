8cf241445dd9e9910725e068cc7f6b463b79817e
reza <ryarbakhsh@yahoo.com>
2024-10-20 18:43:21 +0000

    default mode changed to dark

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const App: React.FC = () => {
 
   const [familyMembers, setFamilyMembers] = useState<FamilyMember[]>([]);
 
-  const [mode, setMode] = useState<'light' | 'dark'>('light');
+  const [mode, setMode] = useState<'light' | 'dark'>('dark');
 
   const colorMode = useMemo(
     () => ({
